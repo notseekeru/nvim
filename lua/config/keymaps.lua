45499920865e838f25f996cdfd4f0b329e528aaa
Stephen Macabulos <myteseeker18@gmail.com>
2026-05-03 07:33:58 +0800

    fix deprecated append harpoon

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,7 +7,7 @@ harpoon:setup()
 
 -- Basic keymaps
 vim.keymap.set("n", "<leader>a", function()
-  harpoon:list():append()
+  harpoon:list():add()
 end)
 vim.keymap.set("n", "<C-e>", function()
   harpoon.ui:toggle_quick_menu(harpoon:list())
