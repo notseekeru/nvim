06a68fe559509d4da3c611d093790941313f26c5
Stephen Macabulos <myteseeker18@gmail.com>
2026-05-04 07:50:39 +0800

    scroll middle and timeout change

--- init.lua
+++ init.lua
@@ -2,3 +2,6 @@
 require("config.lazy")
 vim.keymap.set("n", ";", ":", { noremap = true })
 vim.keymap.set("n", ":", ";", { noremap = true })
+vim.opt.timeoutlen = 100
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
