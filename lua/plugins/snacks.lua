43b1d2e1cc6ac1aef1ca9a0d056a8d12614c3659
Stephen Macabulos <myteseeker18@gmail.com>
2026-05-08 06:53:15 +0800

    removed snacks startup

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -16,13 +16,4 @@ return {
       jump = { close = false },
     },
   },
-  init = function()
-    vim.api.nvim_create_autocmd("VimEnter", {
-      callback = function()
-        if vim.fn.argc() == 0 then
-          Snacks.explorer()
-        end
-      end,
-    })
-  end,
 }
