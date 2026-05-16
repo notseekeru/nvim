78d745a2e7ace1b6bd8e8e6cccd91b0de662a9a8
Stephen Macabulos <myteseeker18@gmail.com>
2026-05-17 06:06:40 +0800

    ui stuff dud wth

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        "echasnovski/mini.icons",
+        opts = {
+            file = {
+                ["docker-compose.yml"] = { glyph = "  ", hl = "MiniIconsAzure" },
+                ["docker-compose.yaml"] = { glyph = "  ", hl = "MiniIconsAzure" },
+            },
+        },
+    },
+}
