2ddf5e0189fd1c1e484aebe15a192d1dc3a7bf03
Stephen Macabulos <myteseeker18@gmail.com>
2026-05-18 06:48:48 +0800

    i cant take it anymore

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,1 @@
-vim.api.nvim_create_autocmd("ColorScheme", {
-    pattern = "*",
-    callback = function()
-        vim.api.nvim_set_hl(0, "SnacksPicker", { bg = "#1a1b26", nocombine = true })
-        vim.api.nvim_set_hl(0, "SnacksPickerBorder", { fg = "#7aa2f7", bg = "none", nocombine = true })
-    end,
-})
+
