7a47e846849d9aff031b66ad27ffcd12faaa47ce
Stephen Macabulos <myteseeker18@gmail.com>
2026-06-06 19:25:01 +0800

    awodapowkd

--- init.lua
+++ init.lua
@@ -3,3 +3,9 @@ require("config.lazy")
 
 vim.opt.clipboard = "unnamedplus"
 vim.opt.conceallevel = 0
+
+vim.opt.termguicolors = true
+
+vim.cmd("highlight RedCursor guibg=#FF0000 guifg=#FFFFFF")
+
+vim.opt.guicursor = "n-v-c-i-ci-ve-t-r-cr-o:block-RedCursor"
