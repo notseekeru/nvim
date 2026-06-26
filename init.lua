69ae9033db0c3ef884fb7d6a3fa0a4dded13c338
Stephen Macabulos <myteseeker18@gmail.com>
2026-06-26 23:30:57 +0800

    ref: refactor code

--- init.lua
+++ init.lua
@@ -1,13 +1,10 @@
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -17,292 +14,75 @@ require("plugins")
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-require("nvim-treesitter").setup({
-	highlight = { enable = true },
-})
-
--- kj acts as Escape in insert and visual mode
+-- Keymaps
 vim.keymap.set("i", "kj", "<Esc>")
 vim.keymap.set("v", "kj", "<Esc>")
-
--- s: save file (simple, no error if no alternate buffer)
 vim.keymap.set("n", "s", "<Cmd>write<CR>", { desc = "Save file" })
-
 vim.keymap.set("n", "gg", "gg0")
-
 vim.keymap.set("n", "j", "jzz")
 vim.keymap.set("n", "k", "kzz")
-
 vim.keymap.set("v", "J", "j")
-
 vim.keymap.set("v", "K", "k")
-
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-e>", "<C-u>zz", { noremap = true, silent = true })
-
 vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move up" })
 vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move down" })
-
 vim.keymap.set("i", "<A-j>", "<Esc>:m .+1<CR>==gi", { desc = "Move down" })
 vim.keymap.set("i", "<A-k>", "<Esc>:m .-2<CR>==gi", { desc = "Move up" })
-
 vim.keymap.set("n", "<C-a>", ":<C-u>normal! ggVG<CR>", { silent = true })
-
 vim.keymap.set("x", "<leader>p", '"_dP')
-
 vim.keymap.set("v", "<leader>y", '"+y')
 vim.keymap.set("n", "<leader>y", '"+y')
 
+-- Plugins Keymaps
+vim.keymap.set("n", "<leader>ff", function() require("snacks").picker.files() end, { desc = "Find Files" })
+vim.keymap.set("n", "<leader>fg", function() require("snacks").picker.grep() end, { desc = "Grep" })
+vim.keymap.set("n", "<leader>fb", function() require("snacks").picker.buffers() end, { desc = "Buffers" })
+vim.keymap.set("n", "<leader>fh", function() require("snacks").picker.help() end, { desc = "Help Pages" })
+vim.keymap.set("n", "<leader>fk", function() require("snacks").picker.keymaps() end, { desc = "Keymaps" })
+vim.keymap.set("n", "<leader>fr", function() require("snacks").picker.recent() end, { desc = "Recent Files" })
+vim.keymap.set("n", "<leader>fs", function() require("snacks").picker.lsp_symbols() end, { desc = "LSP Symbols" })
+vim.keymap.set("n", "<leader>fS", function() require("snacks").picker.lsp_workspace_symbols() end, { desc = "LSP Workspace Symbols" })
+vim.keymap.set("n", "<leader>fw", function() require("snacks").picker.grep_word() end, { desc = "Grep Word" })
 
-require("codediff").setup({})
+vim.keymap.set("n", "<leader>ha", function() require("harpoon"):list():add() end, { desc = "Harpoon: Add file" })
+vim.keymap.set("n", "<leader>he", function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end, { desc = "Harpoon: Quick menu" })
+vim.keymap.set("n", "<leader>h1", function() require("harpoon"):list():select(1) end, { desc = "Harpoon: File 1" })
+vim.keymap.set("n", "<leader>h2", function() require("harpoon"):list():select(2) end, { desc = "Harpoon: File 2" })
+vim.keymap.set("n", "<leader>h3", function() require("harpoon"):list():select(3) end, { desc = "Harpoon: File 3" })
+vim.keymap.set("n", "<leader>h4", function() require("harpoon"):list():select(4) end, { desc = "Harpoon: File 4" })
 
-require("blink-cmp").setup({
-	keymap = {
-		preset = "default",
-		["<C-Space>"] = { "show", "show_documentation" },
-		["<C-e>"] = { "hide" },
-		["<Tab>"] = { "accept", "fallback" },
-		["<CR>"] = { "fallback" },
-	},
-	sources = {
-		default = { "lsp", "path", "snippets", "buffer" },
-	},
-	fuzzy = { implementation = "prefer_rust_with_warning" },
-})
+vim.keymap.set("n", "<leader>bh", function() require("bufferline").cycle(-1) end, { desc = "Previous buffer" })
+vim.keymap.set("n", "<leader>bl", function() require("bufferline").cycle(1) end, { desc = "Next buffer" })
+vim.keymap.set("n", "<leader>bj", function() require("bufferline").move(-1) end, { desc = "Move buffer tab left" })
+vim.keymap.set("n", "<leader>bk", function() require("bufferline").move(1) end, { desc = "Move buffer tab right" })
+vim.keymap.set("n", "<leader>bd", function() vim.api.nvim_buf_delete(0, {}) end, { desc = "Close buffer" })
+vim.keymap.set("n", "<leader>bo", function() require("bufferline").close_others() end, { desc = "Close other buffers" })
 
-require("snacks").setup({
-	bigfile = { enabled = true },
-	explorer = { enabled = true },
-	indent = { enabled = true },
-	input = { enabled = true },
-	notifier = { enabled = true, style = "compact" },
-	picker = {
-		enabled = true,
-		sources = {
-			explorer = {
-				hidden = true,
-				ignored = true,
-			},
-			files = {
-				hidden = true,
-				ignored = true,
-			},
-		},
-	},
-	quickfile = { enabled = true },
-	scroll = { enabled = true },
-	statuscolumn = { enabled = true },
-	words = { enabled = true },
-})
+vim.keymap.set("n", "<leader>e", function() require("snacks").explorer() end, { desc = "Snacks: Toggle explorer" })
+vim.keymap.set("n", "<leader>E", function() require("snacks").explorer({ layout = { position = "right", width = 40 } }) end, { desc = "Snacks: Explorer right panel" })
+vim.keymap.set("n", "<leader>gg", function() require("snacks").terminal("lazygit", { cwd = vim.fn.getcwd() }) end, { desc = "Lazygit" })
 
-vim.keymap.set("n", "<leader>ff", function()
-	require("snacks").picker.files()
-end, { desc = "Find Files" })
-vim.keymap.set("n", "<leader>fg", function()
-	require("snacks").picker.grep()
-end, { desc = "Grep" })
-vim.keymap.set("n", "<leader>fb", function()
-	require("snacks").picker.buffers()
-end, { desc = "Buffers" })
-vim.keymap.set("n", "<leader>fh", function()
-	require("snacks").picker.help()
-end, { desc = "Help Pages" })
-vim.keymap.set("n", "<leader>fk", function()
-	require("snacks").picker.keymaps()
-end, { desc = "Keymaps" })
-vim.keymap.set("n", "<leader>fr", function()
-	require("snacks").picker.recent()
-end, { desc = "Recent Files" })
-vim.keymap.set("n", "<leader>fs", function()
-	require("snacks").picker.lsp_symbols()
-end, { desc = "LSP Symbols" })
-vim.keymap.set("n", "<leader>fS", function()
-	require("snacks").picker.lsp_workspace_symbols()
-end, { desc = "LSP Workspace Symbols" })
-vim.keymap.set("n", "<leader>fw", function()
-	require("snacks").picker.grep_word()
-end, { desc = "Grep Word" })
+vim.keymap.set({ "n", "x", "o" }, "<leader>j", function() require("flash").jump() end, { desc = "Flash: Jump" })
+vim.keymap.set({ "n", "x", "o" }, "<leader>J", function() require("flash").treesitter() end, { desc = "Flash: Treesitter" })
 
-local harpoon = require("harpoon")
-harpoon:setup({})
+vim.keymap.set({ "n", "v" }, "<leader>F", function() require("conform").format({ lsp_fallback = true, async = false, timeout_ms = 1000 }) end, { desc = "Format" })
 
-vim.keymap.set("n", "<leader>ha", function()
-	harpoon:list():add()
-end, { desc = "Harpoon: Add file" })
-vim.keymap.set("n", "<leader>he", function()
-	harpoon.ui:toggle_quick_menu(harpoon:list())
-end, { desc = "Harpoon: Quick menu" })
-vim.keymap.set("n", "<leader>h1", function()
-	harpoon:list():select(1)
-end, { desc = "Harpoon: File 1" })
-vim.keymap.set("n", "<leader>h2", function()
-	harpoon:list():select(2)
-end, { desc = "Harpoon: File 2" })
-vim.keymap.set("n", "<leader>h3", function()
-	harpoon:list():select(3)
-end, { desc = "Harpoon: File 3" })
-vim.keymap.set("n", "<leader>h4", function()
-	harpoon:list():select(4)
-end, { desc = "Harpoon: File 4" })
-
--- bufferline.nvim: Chrome-like tabs at the top
-require("bufferline").setup({
-	options = {
-		-- Buffer numbering: none, ordinal, buffer_id, both
-		numbers = "none",
-		tab_size = 18,
-		separator_style = "slant",
-		offsets = {
-			{
-				filetype = "NvimTree",
-				text = "Explorer",
-				padding = 1,
-			},
-		},
-		buffer_close_icon = "󰅖",
-		close_command = "bdelete! %d",
-		right_mouse_command = "bdelete! %d",
-		left_mouse_command = "buffer %d",
-		middle_mouse_command = nil,
-		close_icon = "",
-		show_buffer_icons = true,
-		show_close_icon = false,
-		show_tab_indicators = true,
-		indicator = {
-			icon = "▎",
-			style = "icon",
-		},
-	},
-})
-
--- Keymaps
-vim.keymap.set("n", "<leader>bh", function()
-	require("bufferline").cycle(-1)
-end, { desc = "Previous buffer" })
-vim.keymap.set("n", "<leader>bl", function()
-	require("bufferline").cycle(1)
-end, { desc = "Next buffer" })
-vim.keymap.set("n", "<leader>bj", function()
-	require("bufferline").move(-1)
-end, { desc = "Move buffer tab left" })
-vim.keymap.set("n", "<leader>bk", function()
-	require("bufferline").move(1)
-end, { desc = "Move buffer tab right" })
-vim.keymap.set("n", "<leader>bd", function()
-	vim.api.nvim_buf_delete(0, {})
-end, { desc = "Close buffer" })
-vim.keymap.set("n", "<leader>bo", function()
-	require("bufferline").close_others()
-end, { desc = "Close other buffers" })
-
-vim.keymap.set("n", "<leader>e", function()
-	require("snacks").explorer()
-end, { desc = "Snacks: Toggle explorer" })
-
-vim.keymap.set("n", "<leader>E", function()
-	require("snacks").explorer({
-		layout = {
-			position = "right",
-			width = 40,
-		},
-	})
-end, { desc = "Snacks: Explorer right panel" })
-
-vim.keymap.set("n", "<leader>gg", function()
-	require("snacks").terminal("lazygit", { cwd = vim.fn.getcwd() })
-end, { desc = "Lazygit" })
-
-require("flash").setup({
-	modes = {
-		search = { enabled = false },
-		char = { enabled = true },
-		treesitter = { enabled = true },
-	},
-	jump = {
-		autojump = false,
-		history = false,
-		register = false,
-	},
-})
-
-vim.keymap.set({ "n", "x", "o" }, "<leader>j", function()
-	require("flash").jump()
-end, { desc = "Flash: Jump" })
-vim.keymap.set({ "n", "x", "o" }, "<leader>J", function()
-	require("flash").treesitter()
-end, { desc = "Flash: Treesitter" })
-
-require("conform").setup({
-	formatters_by_ft = {
-		go = { "lsp" },
-		rust = { "lsp" },
-		nix = { "nixpkgs-fmt" },
-		lua = { "stylua" },
-		python = { "ruff" },
-		javascript = { "prettierd", "prettier", stop_after_first = true },
-		typescript = { "prettierd", "prettier", stop_after_first = true },
-		javascriptreact = { "prettierd", "prettier", stop_after_first = true },
-		typescriptreact = { "prettierd", "prettier", stop_after_first = true },
-		json = { "prettierd", "prettier", stop_after_first = true },
-		yaml = { "yamlfmt" },
-		bash = { "shfmt" },
-		dockerfile = { "hadolint" },
-		markdown = { "prettierd", "prettier", stop_after_first = true },
-		["_"] = { "lsp" },
-	},
-	format_on_save = {
-		lsp_fallback = true,
-		timeout_ms = 1000,
-	},
-})
-
-vim.keymap.set({ "n", "v" }, "<leader>F", function()
-	require("conform").format({
-		lsp_fallback = true,
-		async = false,
-		timeout_ms = 1000,
-	})
-end, { desc = "Format" })
-
--- Ctrl+S: leave insert mode, save, then format
 vim.keymap.set("i", "<C-s>", function()
 	vim.cmd("stopinsert")
 	vim.cmd("silent write")
-	require("conform").format({
-		lsp_fallback = true,
-		async = false,
-		timeout_ms = 1000,
-	})
+	require("conform").format({ lsp_fallback = true, async = false, timeout_ms = 1000 })
 end, { desc = "Leave insert, save & format" })
 
 vim.keymap.set("n", "<C-s>", function()
 	vim.cmd("silent write")
-	require("conform").format({
-		lsp_fallback = true,
-		async = false,
-		timeout_ms = 1000,
-	})
+	require("conform").format({ lsp_fallback = true, async = false, timeout_ms = 1000 })
 end, { desc = "Save & format" })
 
+-- Options
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.termguicolors = true
-
--- Colorscheme
-require("catppuccin").setup({
-	flavour = "mocha", -- mocha, macchiato, frappe, latte
-	term_colors = true,
-	integrations = {
-		treesitter = true,
-		cmp = true,
-		snacks = true,
-		bufferline = true,
-		harpoon = true,
-		flash = true,
-		lsp_trouble = true,
-	},
-})
-vim.cmd.colorscheme("catppuccin")
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
@@ -313,7 +93,7 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.updatetime = 250
 
--- Autosave: save automatically on certain events
+-- Autocmds
 vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged", "TextChangedI" }, {
 	pattern = "*",
 	callback = function()
@@ -323,13 +103,6 @@ vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged", "TextChangedI" }, {
 	end,
 })
 
--- persistence.nvim: auto-save/restore session (like LazyVim)
-require("persistence").setup({
-	options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals" },
-	need = 0,
-})
-
--- Auto-restore session on startup (only if nvim opened without file args)
 vim.api.nvim_create_autocmd("VimEnter", {
 	group = vim.api.nvim_create_augroup("persistence_autoload", { clear = true }),
 	nested = true,
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,26 +1,98 @@
--- lua/plugins.lua
 return require("lazy").setup({
-	"nvim-lua/plenary.nvim",
-	"nvim-treesitter/nvim-treesitter",
-	"folke/snacks.nvim",
-	"folke/flash.nvim",
-	"folke/persistence.nvim",
-	"ThePrimeagen/harpoon",
-	"akinsho/bufferline.nvim",
-	"stevearc/conform.nvim",
-	"catppuccin/nvim",
-	"esmuellert/codediff.nvim",
-	"f-person/git-blame.nvim",
-	{
-		"saghen/blink.cmp",
-		version = "*",
-	},
-	"williamboman/mason.nvim",
-	"williamboman/mason-lspconfig.nvim",
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			require("lsp")
-		end,
-	},
+    "nvim-lua/plenary.nvim",
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                highlight = { enable = true },
+                ensure_installed = { "lua", "vim", "vimdoc", "query", "go", "python", "rust", "javascript", "typescript" },
+                auto_install = true,
+            })
+        end,
+    },
+    {
+        "folke/snacks.nvim",
+        config = function()
+            require("snacks").setup({
+                bigfile = { enabled = true },
+                explorer = { enabled = true },
+                indent = { enabled = true },
+                input = { enabled = true },
+                notifier = { enabled = true, style = "compact" },
+                picker = { enabled = true },
+                quickfile = { enabled = true },
+                scroll = { enabled = true },
+                statuscolumn = { enabled = true },
+                words = { enabled = true },
+            })
+        end,
+    },
+    {
+        "saghen/blink.cmp",
+        version = "*",
+        config = function()
+            require("blink.cmp").setup({
+                keymap = { preset = "default" },
+                sources = { default = { "lsp", "path", "snippets", "buffer" } },
+            })
+        end,
+    },
+    {
+        "catppuccin/nvim",
+        config = function()
+            require("catppuccin").setup({ flavour = "mocha" })
+            vim.cmd.colorscheme("catppuccin")
+        end,
+    },
+    {
+        "folke/flash.nvim",
+        config = function()
+            require("flash").setup({
+                modes = { search = { enabled = false }, char = { enabled = true }, treesitter = { enabled = true } },
+            })
+        end,
+    },
+    {
+        "folke/persistence.nvim",
+        config = function()
+            require("persistence").setup({ options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals" } })
+        end,
+    },
+    {
+        "ThePrimeagen/harpoon",
+        config = function()
+            require("harpoon").setup({})
+        end,
+    },
+    {
+        "akinsho/bufferline.nvim",
+        config = function()
+            require("bufferline").setup({ options = { numbers = "none", tab_size = 18, separator_style = "slant" } })
+        end,
+    },
+    {
+        "stevearc/conform.nvim",
+        config = function()
+            require("conform").setup({
+                formatters_by_ft = {
+                    go = { "lsp" },
+                    rust = { "lsp" },
+                    nix = { "nixpkgs-fmt" },
+                    lua = { "stylua" },
+                    python = { "ruff" },
+                    ["_"] = { "lsp" },
+                },
+                format_on_save = { lsp_fallback = true, timeout_ms = 1000 },
+            })
+        end,
+    },
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require("lua.lsp")
+        end,
+    },
 })
